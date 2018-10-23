d163543f0ec1aa6a0029dec3c065209561add77b
xiaofanshao <404457041@qq.com>
2018-10-23 17:05:34 +0800

    两个界面可以切换了

--- app/src/main/java/com/example/hasee/firstapp/SelectCity.java
+++ app/src/main/java/com/example/hasee/firstapp/SelectCity.java
@@ -2,14 +2,32 @@ package com.example.hasee.firstapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class SelectCity extends Activity {
+public class SelectCity extends Activity implements View.OnClickListener {
+
+    private ImageView mBackBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.select_city);
 
+        mBackBtn=(ImageView)findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_back:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
